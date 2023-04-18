0cd11fb3e75a947d76da980e1b29cb25b291bfba
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2023-04-18 18:01:29 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,54 +1,72 @@
 var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
 
+var cloud, cloudsGroup, cloudImage;
+var newImage;
+
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadImage("trex_collided.png");
+  trex_collided = loadAnimation("trex_collided.png");
   
-  groundImage = loadImage("ground2.png")
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+ 
 }
 
 function setup() {
-  createCanvas(600,200);
-  
-  
-  //crie um sprite de trex
+  createCanvas(600, 200);
+
   trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
+  // trex.adicionarAnimação("colidiu",trex_colidiu)
   trex.scale = 0.5;
   
-  //crie um sprite ground (solo)
-  ground = createSprite(300,180,600,2);
+  ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
-
+  ground.velocityX = -4;
   
-  //crie um solo invisível
-  invisibleGround = createSprite(200,190,600,2);
+  invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
   
+  // var rand = Math.round(random(0,100))
+  // console.log(rand);
 }
 
 function draw() {
-  //definir cor de fundo
-  background(220);
-  ground.velocityX = -4;
-  console.log(trex.y)
+  background(180);
   
-  //pular quando tecla espaço for pressionada
-  if(keyDown("space") && trex.y >= 100) {
+  //console.log(frameCount);
+  
+  if(keyDown("space") && trex.y>=100) {
     trex.velocityY = -10;
   }
   
-  //adicionar gravidade
   trex.velocityY = trex.velocityY + 0.8
   
   if (ground.x < 0){
     ground.x = ground.width/2;
   }
   
-  //impedir que o trex caia
   trex.collide(invisibleGround);
+  
+  //gerar nuvens
+  spawnClouds();
   
   drawSprites();
 }
+
+function spawnClouds() {
+  //escreva aqui o código para gerar nuvens
+  if (frameCount % 80 === 0) {
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage)
+    cloud.y = Math.round(random(10,60))
+    cloud.scale = 0.4;
+    cloud.velocityX = -3;
+    
+    
+    }
+}
+
